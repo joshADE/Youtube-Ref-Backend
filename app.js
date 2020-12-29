370c8938edab96c638ffe385261569cf7a916bd8
Josh A <joshmail9510@gmail.com>
2020-12-29 11:39:22 -0500

    Preparing for deployment to heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const path = require('path'); 
 
 require('dotenv').config();
 
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.MONGODB_URI || 'mongodb://localhost/yt-app';
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
 
 const connection = mongoose.connection;
@@ -22,6 +22,14 @@ connection.once('open', () => {
 app.use('/users', require('./routes/userRoutes'));
 app.use('/videos', require('./routes/videoRoute'));
 
+if (process.env.NODE_ENV === 'production') {
+    // Embedded React app here
+    // app.use(express.static('client/build'));
+
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+    // })
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
